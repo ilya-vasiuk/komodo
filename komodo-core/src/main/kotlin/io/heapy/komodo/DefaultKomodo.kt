682f1dbab9eb49da3c15078737108b8fc3744e34
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-10-17 21:15:06 +0300

    Core module doesn't have dependency on di module.

--- komodo-core/src/main/kotlin/io/heapy/komodo/DefaultKomodo.kt
+++ komodo-core/src/main/kotlin/io/heapy/komodo/DefaultKomodo.kt
@@ -2,31 +2,31 @@ package io.heapy.komodo
 
 import kotlin.reflect.KClass
 
-/**
- * @author Ibragimov Ruslan
- * @since 0.1
- */
-class DefaultKomodo : io.heapy.komodo.Komodo {
-    private val modules = mutableListOf<Module>()
-    private val args = mutableListOf<String>()
-    private val env = mutableMapOf<String, String>()
-
-    override fun module(module: Module): io.heapy.komodo.Komodo {
-        modules += module
-        return this
-    }
-
-    override fun args(args: Array<String>): io.heapy.komodo.Komodo {
-        this.args += args
-        return this
-    }
-
-    override fun env(env: Map<String, String>): io.heapy.komodo.Komodo {
-        this.env += env
-        return this
-    }
-
-    override suspend fun <R> run(entryPoint: KClass<out EntryPoint<R>>): R {
-        return entryPoint.objectInstance!!.run()
-    }
-}
+///**
+// * @author Ibragimov Ruslan
+// * @since 0.1
+// */
+//class DefaultKomodo : io.heapy.komodo.Komodo {
+//    private val modules = mutableListOf<Module>()
+//    private val args = mutableListOf<String>()
+//    private val env = mutableMapOf<String, String>()
+//
+//    override fun module(module: Module): io.heapy.komodo.Komodo {
+//        modules += module
+//        return this
+//    }
+//
+//    override fun args(args: Array<String>): io.heapy.komodo.Komodo {
+//        this.args += args
+//        return this
+//    }
+//
+//    override fun env(env: Map<String, String>): io.heapy.komodo.Komodo {
+//        this.env += env
+//        return this
+//    }
+//
+//    override suspend fun <R> run(entryPoint: KClass<out EntryPoint<R>>): R {
+//        return entryPoint.objectInstance!!.run()
+//    }
+//}
