d580d4145d5841161a6a2eb1831484c86bdc88cb
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-10-17 21:52:15 +0300

    Hardcode dev version for now.

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,7 @@ tag)
   ./gradlew bintrayUpload -Pversion="${version}"
   ;;
 dev)
-  tag=$(git describe --tags $(git rev-list --tags --max-count=1))
-  version="${tag}-dev-b${TRAVIS_BUILD_NUMBER}"
+  version="0.0.1-dev-b${TRAVIS_BUILD_NUMBER}"
   echo "Uploading artifacts with version ${version}"
   ./gradlew bintrayUpload -Pversion="${version}"
   ;;
