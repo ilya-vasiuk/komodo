e85d80f983b4fba95acc730c9a1e5a71f40f6e8a
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-10-17 10:00:37 +0300

    Publish slf4j module.

---
+++ komodo-integration/komodo-slf4j/build.gradle.kts
@@ -0,0 +1,7 @@
+group = "io.heapy.komodo.integration"
+
+apply(from = "$rootDir/publish.gradle")
+
+dependencies {
+    compile("org.slf4j:slf4j-api")
+}
---
+++ komodo-integration/komodo-slf4j/src/main/kotlin/app/hea/integration/slf4j/Logger.kt
@@ -0,0 +1,12 @@
+package app.hea.integration.slf4j
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Function to retrieve logger by class.
+ *
+ * @author Ibragimov Ruslan
+ * @since 0.1
+ */
+inline fun <reified T : Any> logger(): Logger = LoggerFactory.getLogger(T::class.java)
